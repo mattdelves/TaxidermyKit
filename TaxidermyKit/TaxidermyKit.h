bb68857deed366eaefa0aa927277b4600b8fe8d0
Matt Delves <matt.delves@outware.com.au>
2015-03-10 18:15:11 +1100

    Initial Commit

---
+++ TaxidermyKit/TaxidermyKit.h
@@ -0,0 +1,19 @@
+//
+//  TaxidermyKit.h
+//  TaxidermyKit
+//
+//  Created by Matt Delves on 10/03/2015.
+//  Copyright (c) 2015 Reformed Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaxidermyKit.
+FOUNDATION_EXPORT double TaxidermyKitVersionNumber;
+
+//! Project version string for TaxidermyKit.
+FOUNDATION_EXPORT const unsigned char TaxidermyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaxidermyKit/PublicHeader.h>
+
+
